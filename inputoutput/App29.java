f9815f303261472b64918fa3cb9d16945e46d6c4
Sergei Visotsky <me@sergeivisotsky.com>
2017-11-20 20:44:45 +0200

    Build 1

---
+++ inputoutput/App29.java
@@ -0,0 +1,10 @@
+package inputoutput;
+
+public class App29 {
+    public static void main(String[] args) {
+        int[] height = new int[3];
+        int[] weight = new int[3];
+        inputoutput.inputHeightWeight(weight, height);
+        inputoutput.outputHeightWeight(weight, height);
+    }
+}
