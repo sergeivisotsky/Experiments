3d580ae574d0c0a58e1277ca550a6566ef9e5b3e
Sergei Visotsky <me@sergeivisotsky.com>
2017-12-04 19:32:29 +0200

    New

---
+++ src/RetireCalc.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class RetireCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        /*if (age >= 65) {
+            System.out.print("You are old! Bro! You should go for the retirement! :)");
+        } else {
+            System.out.print("You are pretty young and sexy!!! :)" + "\n" + "You should work at least: " + (65 - age));
+        }*/
+        /**
+         * The same idea but now with the switch statement
+         */
+
+        if (age < 0) {
+            System.out.print("Your age shouldn't be negative number! :))))");
+            return;
+        }
+
+        switch (age) {
+            case 65:
+                System.out.print("You are old! Bro! You should go for the retirement! :)");
+                break;
+            case 0:
+                System.out.println("You are just born! Bro! :)" + "\n" + "You are too young to start working! :)");
+                break;
+                default:
+                    System.out.print("You are pretty young and sexy!!! :)" + "\n" + "You should work at least: " + (65 - age));
+        }
+
+        sc.close();
+    }
+}
