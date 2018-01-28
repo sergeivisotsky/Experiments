57ed96225a385f211a16bbc855d542b6785612d3
Sergei <sergei.visotsky@gmail.com>
2018-01-28 08:51:41 +0200

    Recursion fourth variant added

---
+++ src/lambdaExpressions.java
@@ -0,0 +1,10 @@
+public class lambdaExpressions {
+    public static void execution(Runnable runnable) {
+        System.out.println("Hello space!");
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        execution(() -> System.out.println("Hello bro!!!"));
+    }
+}
