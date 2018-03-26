b550322d2f25aeb1a55be23f13be6bc143d2f618
Sergei <sergei.visotsky@gmail.com>
2018-03-26 10:56:00 +0300

    Testing the new feature of Java 10 - local var variables

--- src/main/java/varTesting.java
+++ src/main/java/varTesting.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class varTesting {
     public static void main(String[] args) {
         var hello = new ArrayList<String>();
+        var string = "Hello bro!";
         hello.add("Hello world!");
         hello.add("Hello Mars!");
         hello.add("Hello Neptun!");
@@ -10,5 +11,6 @@ public class varTesting {
         System.out.println(hello.get(0));
         System.out.println(hello.get(1));
         System.out.println(hello.get(2));
+        System.out.println(string);
     }
 }
