3a645f24cdacaffa1aaf1fb318ba936e696ab8ae
Sergei <sergei.visotsky@gmail.com>
2018-03-27 14:10:52 +0300

    Redundant getters and setters removed

--- src/main/java/CountryList/Country.java
+++ src/main/java/CountryList/Country.java
@@ -10,22 +10,6 @@ public class Country {
         this.countryName = countryName;
     }
 
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
-    }
-
     @Override
     public String toString() {
         return "Country{" +
