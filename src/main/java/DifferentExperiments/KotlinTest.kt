179d9069516c57fcf8fac6438f6eb1ef76940912
Sergei <sergei.visotsky@gmail.com>
2018-03-27 10:37:09 +0300

    Kotlin experiment

--- src/main/java/DifferentExperiments/KotlinTest.kt
+++ src/main/java/DifferentExperiments/KotlinTest.kt
@@ -1,12 +1,15 @@
 package DifferentExperiments
 
-val developer = 50;
-val tester = 50;
-fun hellowWorld() {
-    val sum = developer + tester;
-    print(sum);
+class HelloWorld {
+    val developer = 50;
+    val tester = 50;
+    fun hellowWorld() {
+        val sum = developer + tester;
+        print(sum);
+    }
 }
 
 fun main(args: Array<String>) {
-    hellowWorld();
+    val hello = HelloWorld();
+    hello.hellowWorld();
 }
