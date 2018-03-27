c58c7a85e84115f5277e3f6d78cbf6297df81d5d
Sergei <sergei.visotsky@gmail.com>
2018-03-27 13:24:59 +0300

    getters and setters added

--- src/main/java/CountryList/Country.java
+++ src/main/java/CountryList/Country.java
@@ -15,8 +15,16 @@ public class Country {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getLanguage() {
         return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
     }
 
     @Override
