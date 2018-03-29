ad5b8b5308048060bbb6d4a5e96800d5700899ee
Sergei <sergei.visotsky@gmail.com>
2018-03-29 19:28:01 +0300

    db constants added

--- src/main/java/SpringStudy/db/DbConstants.java
+++ src/main/java/SpringStudy/db/DbConstants.java
@@ -1,4 +1,9 @@
 package SpringStudy.db;
 
 public class DbConstants {
+    public static final String DB_FILE_NAME = "main_data";
+    public static final String DB_TABLE_NAME = "human_data";
+    public static final String TABLE_NAME = "name";
+    public static final String TABLE_SURNAME = "surname";
+    public static final String TABLE_AGE = "age";
 }
