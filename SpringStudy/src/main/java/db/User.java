ff3d57db2b1e8eaef78ea082561646c2c079c247
Sergei <sergei.visotsky@gmail.com>
2018-03-30 09:13:33 +0300

    Database connection with Spring Boot framework

--- SpringStudy/src/main/java/db/User.java
+++ SpringStudy/src/main/java/db/User.java
@@ -1,30 +1,31 @@
 package db;
 
 public class User {
-    private String name;
-    private String surname;
+
+    private String firstName;
+    private String lastName;
     private int age;
 
-    public User(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getSurname() {
-        return surname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getAge() {
