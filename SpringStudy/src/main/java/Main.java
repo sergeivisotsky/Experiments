4a56d866bfc2b829c6c35df5b27520600f797b90
Sergei <sergei.visotsky@gmail.com>
2018-03-31 14:31:15 +0300

    Spring start class created

--- SpringStudy/src/main/java/Main.java
+++ SpringStudy/src/main/java/Main.java
@@ -1,25 +1,14 @@
-import db.DbHandler;
 import db.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+@SpringBootApplication
 public class Main {
-    public BufferedReader input =
-            new BufferedReader(new InputStreamReader(System.in));
-
-    public void insertUserData() throws IOException {
-        System.out.print("db. User name: ");
-        String userName = input.readLine();
-        System.out.print("db. User surname: ");
-        String userSurname = input.readLine();
-        System.out.print("db. User age: ");
-        int userAge = Integer.parseInt(input.readLine());
-        User user = new User(userName, userSurname, userAge);
-    }
     public static void main(String[] args) throws IOException {
-        Main main = new Main();
-        main.insertUserData();
+        SpringApplication.run(Main.class, args);
     }
 }
