7c1fa898f259403ed030d62baf5f01cb99dd77f5
Sergei <sergei.visotsky@gmail.com>
2018-04-01 08:42:38 +0300

    Thread experiment added with unit testing

--- src/test/java/Threads/ThreadImplementationTest.java
+++ src/test/java/Threads/ThreadImplementationTest.java
@@ -2,11 +2,9 @@ package Threads;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ThreadImplementationTest {
     @Test
-    private void ThreadImplementation() throws Exception {
+    public void ThreadImplementation() throws Exception {
         Thread.currentThread().setName("Thread study testing");
         Thread testThread = new Thread(new ThreadImplementation(20));
         System.out.println("Thread created!!!");
