d8afc0acf9ededf9b772498498e14f7a9fdbbe53
Sergei <sergei.visotsky@gmail.com>
2018-04-02 21:08:52 +0300

    unhandled exception removed

--- SpringStudy/src/main/java/Main.java
+++ SpringStudy/src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
