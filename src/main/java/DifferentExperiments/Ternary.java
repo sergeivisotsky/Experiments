0c89471ba5155b08a14af3c7b6f8834f37f9245d
Sergei <sergei.visotsky@gmail.com>
2018-04-04 20:01:31 +0300

    Ternary experiment added

---
+++ src/main/java/DifferentExperiments/Ternary.java
@@ -0,0 +1,9 @@
+package DifferentExperiments;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = (a == 5) ? 1 : 0;
+        System.out.println(b);
+    }
+}
