3ecb5b8b5e84fb87fffe3cd1f24bfd5853fb9c9a
Sergei <sergei.visotsky@gmail.com>
2018-04-04 06:00:06 +0300

    String experimetn added

---
+++ src/main/java/DifferentExperiments/Strings.java
@@ -0,0 +1,9 @@
+package DifferentExperiments;
+
+public class Strings {
+    public static void main(String[] args) {
+        String hello = "HelloSergei!";
+        System.out.println(hello.substring(0, 5));
+        System.out.println(hello.substring(5, 12));
+    }
+}
