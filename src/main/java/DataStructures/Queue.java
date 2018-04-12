a770ae43b8f9d60a01ece36bccf2c47b38caa913
Sergei <sergei.visotsky@gmail.com>
2018-04-12 12:01:40 +0300

    Queue modification

--- src/main/java/DataStructures/Queue.java
+++ src/main/java/DataStructures/Queue.java
@@ -5,19 +5,65 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+
 public class Queue {
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader =
                 new BufferedReader(
                         new InputStreamReader(System.in));
-        System.out.print("Number of elements: ");
-        int capacity = Integer.parseInt(reader.readLine());
-        ArrayList<String> queue = new ArrayList<>(capacity);
-        for (int i = 0; i <= capacity - 1; i++) {
-            System.out.print("Add " + (i + 1) + " element: ");
-            String element = reader.readLine();
-            queue.add(0, element);
-        }
-        System.out.print("Peek element: " + queue.get(0));
+        int answer;
+        String yesNoAnswer;
+        String element;
+        ArrayList<String> queue = new ArrayList<>();
+        do {
+            System.out.println("1: enQueue");
+            System.out.println("2: Show queue");
+            System.out.println("3: deQueue");
+            System.out.println("4: peek");
+            System.out.println("5: size");
+            System.out.println("6: isEmpty");
+            System.out.print("\nChoose answer: ");
+            answer = Integer.parseInt(reader.readLine());
+            switch (answer) {
+                case 1:
+                    System.out.print("Add element: ");
+                    element = reader.readLine();
+                    queue.add(0, element);
+                    break;
+                case 2:
+                    if (!queue.isEmpty()) {
+                        System.out.println("Created queue: ");
+                        for (int i = 0; i < queue.size() - 1; i++) {
+                            System.out.print(queue.get(i) + "\t");
+                        }
+                    } else {
+                        throw new IllegalStateException("Queue is empty!");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Deletable element: " +
+                            queue.remove(0));
+                    break;
+                case 4:
+                    System.out.print("Peeked element: " + queue.get(0));
+                    break;
+                case 5:
+                    if (!queue.isEmpty()) {
+                        System.out.println("Queue size is: " + queue.size());
+                    } else {
+                        throw new IllegalStateException("Queue is empty!");
+                    }
+                    break;
+                case 6:
+                    System.out.println("Empty status: " +
+                            queue.isEmpty());
+                    break;
+                default:
+                    throw new IllegalStateException("Illegal state!");
+            }
+            System.out.print("\nContinue? (y/n) ");
+            yesNoAnswer = reader.readLine();
+        } while (yesNoAnswer.equals("y") || yesNoAnswer.equals("Y"));
     }
 }
