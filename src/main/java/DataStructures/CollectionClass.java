4f0df873cead099f98017b958e45fdeedf3c4594
Sergei <sergei.visotsky@gmail.com>
2018-04-16 21:19:43 +0300

    Colection collectiona added

---
+++ src/main/java/DataStructures/CollectionClass.java
@@ -0,0 +1,14 @@
+package DataStructures;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionClass {
+
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("Hello!");
+        collection.add("World!");
+        System.out.println(collection.contains("Hello!"));
+    }
+}
