9d4f2c8e55f9dd88682a6ad39154ee359bec7f7f
Sergei <sergei.visotsky@gmail.com>
2018-04-22 16:59:28 +0300

    Epsilon calculator latest build (have bugs)

--- src/main/java/DifferentExperiments/EpsilonCalc.java
+++ src/main/java/DifferentExperiments/EpsilonCalc.java
@@ -3,8 +3,20 @@ package DifferentExperiments;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class EpsilonCalc {
+    static double average(int quantity, double element) {
+        LinkedList<Double> elements = new LinkedList<>();
+        elements.add(element);
+        double result = 0;
+        for (int i = 0; i < quantity; i++) {
+            result += elements.get(i);
+        }
+
+        return result / quantity;
+    }
+
     static double epsilon(double delta, double average) {
         return (delta / average) * 100;
     }
@@ -14,10 +26,18 @@ public class EpsilonCalc {
                 new BufferedReader(
                         new InputStreamReader(System.in));
         try {
+
+            System.out.print("Quantity of average numbers: ");
+            int quantity = Integer.parseInt(input.readLine());
+            double average = 0;
+            for (int i = 0; i < quantity; i++) {
+                System.out.print("Number: ");
+                double number = Double.parseDouble(input.readLine());
+                average = average(quantity, number);
+            }
+
             System.out.print("Delta: ");
             double delta = Double.parseDouble(input.readLine());
-            System.out.print("Average: ");
-            double average = Double.parseDouble(input.readLine());
             System.out.print("ε = ");
             System.out.printf("%.1f", epsilon(delta, average));
             System.out.print("%");
--- src/test/java/DifferentExperiments/EpsilonCalcTest.java
+++ src/test/java/DifferentExperiments/EpsilonCalcTest.java
@@ -6,6 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EpsilonCalcTest {
 
+    private double insertionForTest() {
+        int quantity = 3;
+        double result = 0;
+        for (int i = 0; i < quantity; i++) {
+            result = EpsilonCalc.average(3, 2);
+        }
+        return result;
+    }
+
+    @Test
+    void averageTest() {
+        assertEquals(4, insertionForTest());
+    }
+
     @Test
     void epsilonTest() {
         assertEquals(7.6923076923076925,
