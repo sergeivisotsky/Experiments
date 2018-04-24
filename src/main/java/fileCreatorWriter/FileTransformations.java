a09850c18b19b50d9876ebbbaf722a3c4a5a4095
Sergei <sergei.visotsky@gmail.com>
2018-04-24 21:27:49 +0300

    File transformator saves date into the file

--- src/main/java/fileCreatorWriter/FileTransformations.java
+++ src/main/java/fileCreatorWriter/FileTransformations.java
@@ -1,13 +1,17 @@
 package fileCreatorWriter;
 
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class FileTransformations {
     private final String PATH = "D:\\Users\\Sergei\\Documents\\JavaProjects\\" +
             "Experiments\\src\\main\\java\\fileCreatorWriter\\createdFiles\\";
     private final String FILE_NAME = "readable.txt";
     private File file = new File(PATH + FILE_NAME);
-
+    
     private void readFile() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String output;
@@ -29,7 +33,9 @@ public class FileTransformations {
         BufferedReader reader =
                 new BufferedReader(
                         new InputStreamReader(System.in));
-
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date today = Calendar.getInstance().getTime();
+        String reportDate = dateFormat.format(today);
         // Reading from file
         fileTransformations.readFile();
         // Entering to file
@@ -37,6 +43,7 @@ public class FileTransformations {
         String lineToFile = reader.readLine();
         // Writing to file
         fileTransformations.writeToFile(lineToFile);
+        fileTransformations.writeToFile(reportDate);
         System.out.println("Data was saved!");
         //Output file
         System.out.print("Read from file (y/n)?" + "\n" + "Answer: ");
