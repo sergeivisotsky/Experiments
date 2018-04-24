dc11e00c8ad51813a903ad1ad36dd7a8b8b148a7
Sergei <sergei.visotsky@gmail.com>
2018-04-24 21:36:38 +0300

    File transformator update

--- src/main/java/fileCreatorWriter/FileTransformations.java
+++ src/main/java/fileCreatorWriter/FileTransformations.java
@@ -11,7 +11,7 @@ public class FileTransformations {
             "Experiments\\src\\main\\java\\fileCreatorWriter\\createdFiles\\";
     private final String FILE_NAME = "readable.txt";
     private File file = new File(PATH + FILE_NAME);
-    
+
     private void readFile() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String output;
@@ -28,7 +28,14 @@ public class FileTransformations {
         out.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    private void clearAll() throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(file);
+        writer.print("");
+        System.out.println("Data was deleted!");
+        writer.close();
+    }
+
+    public static void main(String[] args) {
         FileTransformations fileTransformations = new FileTransformations();
         BufferedReader reader =
                 new BufferedReader(
@@ -36,20 +43,43 @@ public class FileTransformations {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date today = Calendar.getInstance().getTime();
         String reportDate = dateFormat.format(today);
-        // Reading from file
-        fileTransformations.readFile();
-        // Entering to file
-        System.out.print("\nEnter line to file: ");
-        String lineToFile = reader.readLine();
-        // Writing to file
-        fileTransformations.writeToFile(lineToFile);
-        fileTransformations.writeToFile(reportDate);
-        System.out.println("Data was saved!");
-        //Output file
-        System.out.print("Read from file (y/n)?" + "\n" + "Answer: ");
-        String yesNoAnswer = reader.readLine();
-        if (yesNoAnswer.equals("y") || yesNoAnswer.equals("Y")) {
-            fileTransformations.readFile();
+
+        int answer;
+        String yesNoAnswer;
+
+        try (BufferedReader input =
+                     new BufferedReader(
+                             new InputStreamReader(System.in))) {
+            System.out.println("File transformation");
+
+            do {
+                System.out.println("1: Show data");
+                System.out.println("2: Add record");
+                System.out.println("3: Clear all");
+                System.out.print("\nAnswer: ");
+                answer = Integer.parseInt(input.readLine());
+                switch (answer) {
+                    case 1:
+                        fileTransformations.readFile();
+                        break;
+                    case 2:
+                        System.out.print("\nEnter line to file: ");
+                        String lineToFile = reader.readLine();
+                        fileTransformations.writeToFile(lineToFile);
+                        fileTransformations.writeToFile(reportDate);
+                        System.out.println("Data was saved!");
+                        break;
+                    case 3:
+                        fileTransformations.clearAll();
+                        break;
+                    default:
+                        throw new IllegalStateException("Illegal value!");
+                }
+                System.out.print("\nContinue? (y/n) ");
+                yesNoAnswer = input.readLine();
+            } while (yesNoAnswer.equals("y") || yesNoAnswer.equals("Y"));
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Invalid data format!");
         }
     }
 }
