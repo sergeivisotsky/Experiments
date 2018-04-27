ec170ce25e5ccda86d48a88ed447f3d2240e59cd
Sergei <sergei.visotsky@gmail.com>
2018-04-27 06:14:17 +0300

    Hibernate dependency added

--- SpringStudy/src/main/java/SpringController.java
+++ SpringStudy/src/main/java/SpringController.java
@@ -1,8 +1,6 @@
-import db.User;
 import org.springframework.stereotype.Controller;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 @Controller
@@ -10,13 +8,5 @@ public class SpringController {
     public BufferedReader input =
             new BufferedReader(new InputStreamReader(System.in));
 
-    public void insertUserData() throws IOException {
-        System.out.print("db. User name: ");
-        String userName = input.readLine();
-        System.out.print("db. User surname: ");
-        String userSurname = input.readLine();
-        System.out.print("db. User age: ");
-        int userAge = Integer.parseInt(input.readLine());
-        User user = new User(userName, userSurname, userAge);
-    }
+
 }
