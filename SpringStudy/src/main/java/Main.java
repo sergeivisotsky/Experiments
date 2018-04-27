397cbe2aa7aad52cde846000adb80979037702aa
Sergei <sergei.visotsky@gmail.com>
2018-04-27 15:39:11 +0300

    Refactoring

--- SpringStudy/src/main/java/Main.java
+++ SpringStudy/src/main/java/Main.java
@@ -1,10 +1,5 @@
-import db.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class Main {
--- SpringStudy/src/main/java/SpringController.java
+++ SpringStudy/src/main/java/SpringController.java
@@ -5,5 +5,5 @@ import java.io.InputStreamReader;
 
 @Controller
 public class SpringController {
-    
+
 }
