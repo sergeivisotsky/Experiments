3be6196a9eba36217b13f3a9977978a5d3a9220f
Sergei <sergei.visotsky@gmail.com>
2018-05-06 13:48:02 +0300

    Tasks added

--- src/main/java/Collections/Exercises/LinkedListTasks.java
+++ src/main/java/Collections/Exercises/LinkedListTasks.java
@@ -30,7 +30,40 @@ public class LinkedListTasks {
 
     static class ThirdTask {
         public static void main(String[] args) {
+            LinkedList<Integer> numbers = new LinkedList<>();
+            for (int i = 1; i <= 20; i += 3) {
+                numbers.add(i);
+            }
+            System.out.println("LinkedList before: " + numbers);
+            numbers.addLast(55);
+            System.out.println("LinkedList after: " + numbers);
+        }
+    }
 
+    static class FourthTask {
+        public static void main(String[] args) {
+            LinkedList<Integer> numbers = new LinkedList<>();
+            numbers.add(13);
+            numbers.add(35);
+            numbers.add(13);
+            numbers.add(19);
+            numbers.add(56);
+            System.out.println("LinkedList result: " + numbers);
+            System.out.println("First element: " + numbers.getFirst());
+            System.out.println("Last element: " + numbers.getLast());
+        }
+    }
+
+    static class FifthTask {
+        public static void main(String[] args) {
+            LinkedList<Integer> numbers = new LinkedList<>();
+            for (int i = 1; i < 20; i += 3) {
+                numbers.add(i);
+            }
+            for (int i = 0; i < numbers.size(); i++) {
+                System.out.println("Element-index: " +
+                        numbers.get(i) + " - " + i);
+            }
         }
     }
 }
