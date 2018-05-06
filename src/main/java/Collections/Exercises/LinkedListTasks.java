03911e151873e38a87562e51a98db823118d4308
Sergei <sergei.visotsky@gmail.com>
2018-05-06 13:24:32 +0300

    The next part of tasks deployed

--- src/main/java/Collections/Exercises/LinkedListTasks.java
+++ src/main/java/Collections/Exercises/LinkedListTasks.java
@@ -1,9 +1,36 @@
 package Collections.Exercises;
 
+import java.util.LinkedList;
+
 public class LinkedListTasks {
     static class FirstTask {
         public static void main(String[] args) {
-            
+            LinkedList<Integer> numbers = new LinkedList<>();
+            numbers.add(23);
+            numbers.add(45);
+            numbers.add(55);
+            System.out.println("Linked list before appending: " + numbers);
+            numbers.addLast(36);
+            System.out.println("Linked list after appending: " + numbers);
+        }
+    }
+
+    static class SecondTask {
+        public static void main(String[] args) {
+            LinkedList<String> animals = new LinkedList<>();
+            animals.add("Cat");
+            animals.add("Dog");
+            animals.add("Cheetah");
+            System.out.print("Animals: ");
+            for (String animal : animals) {
+                System.out.print(animal + " ");
+            }
+        }
+    }
+
+    static class ThirdTask {
+        public static void main(String[] args) {
+
         }
     }
 }
