7c420bcf2a93d0499814197bd5b6b0a083f706b6
Sergei <sergei.visotsky@gmail.com>
2018-05-07 21:22:14 +0300

    More tasks on TreeSet added

--- src/main/java/Collections/Exercises/TreeSetTasks.java
+++ src/main/java/Collections/Exercises/TreeSetTasks.java
@@ -1,8 +1,12 @@
 package Collections.Exercises;
 
+import java.util.Iterator;
+import java.util.Random;
 import java.util.TreeSet;
 
 public class TreeSetTasks {
+    private static Random rd = new Random();
+
     static class FirstTask {
         public static void main(String[] args) {
             TreeSet<String> colors = new TreeSet<>();
@@ -10,6 +14,41 @@ public class TreeSetTasks {
             colors.add("green");
             colors.add("blue");
             System.out.println("Colors: " + colors);
+        }
+    }
+
+    static class SecondTask {
+        public static void main(String[] args) {
+            TreeSet<Integer> numbers = new TreeSet<>();
+            for (int i = 0; i < 10; i++) {
+                numbers.add(rd.nextInt(99) + 1);
+            }
+            System.out.print("TreeSet contains: ");
+            for (int element : numbers) {
+                System.out.print(element + "\t");
+            }
+        }
+    }
+
+    static class ThirdTask {
+        public static void main(String[] args) {
+            TreeSet<String> firstSetOfColors = new TreeSet<>();
+            firstSetOfColors.add("red");
+            firstSetOfColors.add("green");
+            firstSetOfColors.add("blue");
+            System.out.println("First TreeSet: " + firstSetOfColors);
+            TreeSet<String> secondSetOfColors = new TreeSet<>();
+            secondSetOfColors.add("purple");
+            secondSetOfColors.add("magenta");
+            secondSetOfColors.add("green");
+            secondSetOfColors.addAll(firstSetOfColors);
+            System.out.println("Second TreeSet: " + secondSetOfColors);
+        }
+    }
+
+    static class FourthTask {
+        public static void main(String[] args) {
+            
         }
     }
 }
