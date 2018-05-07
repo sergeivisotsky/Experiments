69913b31de13d848cca0d1b560d3b695040e8e6b
Sergei <sergei.visotsky@gmail.com>
2018-05-07 06:59:15 +0300

    First task added

--- src/main/java/Collections/Exercises/HashSetTasks.java
+++ src/main/java/Collections/Exercises/HashSetTasks.java
@@ -1,7 +1,19 @@
 package Collections.Exercises;
 
+import java.util.HashSet;
+
 public class HashSetTasks {
     static class FirstTask {
+        public static void main(String[] args) {
+            HashSet<String> names = new HashSet<>();
+            names.add("John");
+            names.add("Maria");
+            names.add("Daniela");
+            System.out.println("HashSet before: " + names);
+        }
+    }
+
+    static class SecondTask {
         public static void main(String[] args) {
             
         }
