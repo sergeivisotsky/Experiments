e15cc651d0283e1ea8bce4fb9f6303db90c73606
Sergei <sergei.visotsky@gmail.com>
2018-05-07 22:16:52 +0300

    More tasks added

--- src/main/java/Collections/Exercises/TreeSetTasks.java
+++ src/main/java/Collections/Exercises/TreeSetTasks.java
@@ -48,7 +48,55 @@ public class TreeSetTasks {
 
     static class FourthTask {
         public static void main(String[] args) {
-            
+            TreeSet<Integer> numbers = new TreeSet<>();
+            for (int i = 0; i < 10; i++) {
+                numbers.add(i);
+            }
+            System.out.println("Numbers TreeSet: " + numbers);
+            Iterator iterate = numbers.descendingIterator();
+            System.out.println("Reversed TreeSet: ");
+            while (iterate.hasNext()) {
+                System.out.print(iterate.next() + "\t");
+            }
+
+            System.out.println("\nThe first element is: " + numbers.first());
+            System.out.println("The last element is: " + numbers.last());
+        }
+    }
+
+    static class FifthTask {
+        public static void main(String[] args) {
+            TreeSet<String> names = new TreeSet<>();
+            names.add("Evelina");
+            names.add("Alina");
+            names.add("Oksana");
+            System.out.println("Names TreeSet: " + names);
+            TreeSet namesClone = (TreeSet) names.clone();
+            System.out.println("Clone of the names TreeSet: " + namesClone);
+            System.out.println("TreeSet size is: " + names.size());
+        }
+    }
+
+    static class SixthTask {
+        public static void main(String[] args) {
+            TreeSet<Integer> numbers = new TreeSet<>();
+            numbers.add(1);
+            numbers.add(13);
+            numbers.add(1445);
+            numbers.add(45);
+            numbers.add(3443);
+            System.out.println("The first TreeSet: " + numbers);
+            TreeSet<Integer> secondNumberSet = new TreeSet<>();
+            secondNumberSet.add(1);
+            secondNumberSet.add(4);
+            secondNumberSet.add(1445);
+            secondNumberSet.add(3);
+            secondNumberSet.add(3443);
+            System.out.println("The second TreeSet: " + secondNumberSet);
+            System.out.print("Contains or not: ");
+            for (int element : numbers) {
+                System.out.print(secondNumberSet.contains(element) ? "Yes\t" : "No\t");
+            }
         }
     }
 }
