70673274a9ce3bec9b37e67c87de5e9ede223f91
Sergei <sergei.visotsky@gmail.com>
2018-05-08 06:47:27 +0300

    Priority queue class and first task added

---
+++ src/main/java/Collections/Exercises/PriorityQueueTasks.java
@@ -0,0 +1,15 @@
+package Collections.Exercises;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueTasks {
+    static class FirstTask {
+        public static void main(String[] args) {
+            PriorityQueue<String> colors = new PriorityQueue<>();
+            colors.add("red");
+            colors.add("green");
+            colors.add("blue");
+            System.out.println("Elements: " + colors);
+        }
+    }
+}
