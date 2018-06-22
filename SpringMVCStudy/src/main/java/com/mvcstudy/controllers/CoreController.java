d64f9e58d490d2764c21db9c8e71d6c300e5df44
Sergei <sergei.visotsky@gmail.com>
2018-06-22 17:38:32 +0300

    Minor changes in Spring MVC experiment

--- SpringMVCStudy/src/main/java/com/mvcstudy/controllers/CoreController.java
+++ SpringMVCStudy/src/main/java/com/mvcstudy/controllers/CoreController.java
@@ -2,12 +2,12 @@ package com.mvcstudy.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CoreController {
 
-    @GetMapping("/main")
+    @RequestMapping("/")
     public String main(String name, Model model) {
         model.addAttribute("test", name);
         return "test app";
