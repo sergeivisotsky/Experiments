486f5eafd00c6afec065f4d0eb09d77c0ccccb71
Sergei <sergei.visotsky@gmail.com>
2018-06-26 18:37:06 +0300

    Interesting task added

---
+++ src/main/java/DifferentExperiments/InterestingTask.java
@@ -0,0 +1,16 @@
+package DifferentExperiments;
+
+public class InterestingTask {
+    public static void main(String[] args) {
+        Integer a = 300;
+        Integer b = 300;
+
+        if (a == b) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
+
