08ddfac93c35c991c2c5e069664cea379b102d67
Sergei <sergei.visotsky@gmail.com>
2018-07-14 21:52:18 +0300

    Three tasks added

---
+++ src/main/java/CppBuzzTasks/SecondTask.java
@@ -0,0 +1,17 @@
+package CppBuzzTasks;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7};
+        int sumEven = 0;
+        int sumOdd = 0;
+        for (int i = 0; i < a.length; i += 3) {
+            sumEven += a[i];
+        }
+        System.out.println("Even: " + sumEven);
+        for (int i = 0; i < a.length; i += 2) {
+            sumOdd += a[i];
+        }
+        System.out.println("Odd: " + sumOdd);
+    }
+}
---
+++ src/main/java/CppBuzzTasks/ThirdTask.java
@@ -0,0 +1,4 @@
+package CppBuzzTasks;
+
+public class ThirdTask {
+}
