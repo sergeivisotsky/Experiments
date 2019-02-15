0e0ee3b56e2b3fcade1441f8f51c38eade8c82ac
Sergei <sergei.visotsky@gmail.com>
2019-02-15 21:16:33 +0200

    Classes renamed

--- Exceptions/src/main/java/org/sergei/exceptions/CheckedException.java
+++ Exceptions/src/main/java/org/sergei/exceptions/CheckedException.java
@@ -3,31 +3,8 @@ package org.sergei.exceptions;
 /**
  * @author Sergei Visotsky
  */
-public class CheckedException {
-
-
-    public static void main(String[] args) throws CheckedExpException {
-        int a = -5;
-        lessThanZeroTry(a);
-//        lessThanZero(a);
+public class CheckedException extends Exception {
+    public CheckedException(String message) {
+        super(message);
     }
-
-    public static void lessThanZero(int a) throws CheckedExpException {
-        if (a < 0) {
-            throw new CheckedExpException("Less than zero");
-        }
-    }
-
-    public static void lessThanZeroTry(int a) {
-        int b = a * 15;
-        try {
-            if (a <= 0) {
-                throw new CheckedExpException("Less than zero");
-            }
-            System.out.println(b);
-        } catch (CheckedExpException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
