5e335d2772d5ac5fe085d3578de59693ecd56392
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-03-10 07:40:01 +0200

    Combo removed, serialUID added

--- exceptions/src/main/java/org/sergei/exceptions/CheckedException.java
+++ exceptions/src/main/java/org/sergei/exceptions/CheckedException.java
@@ -4,6 +4,9 @@ package org.sergei.exceptions;
  * @author Sergei Visotsky
  */
 public class CheckedException extends Exception {
+   
+    private static final long serialVersionUID = 1L;
+
     public CheckedException(String message) {
         super(message);
     }
--- exceptions/src/main/java/org/sergei/exceptions/UncheckedException.java
+++ exceptions/src/main/java/org/sergei/exceptions/UncheckedException.java
@@ -4,6 +4,9 @@ package org.sergei.exceptions;
  * @author Sergei Visotsky
  */
 public class UncheckedException extends RuntimeException {
+    
+    private static final long serialVersionUID = 1L;
+
     public UncheckedException(String message) {
         super(message);
     }
