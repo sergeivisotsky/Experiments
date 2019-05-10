1a7dd6065597494f0a7400c3c99de42767347b3e
Sergei Visotsky <sergei.visotsky@gmail.com>
2019-05-10 07:57:53 +0300

    Submodules moved to the other repo

--- collections/src/main/java/org/sergei/collections/graph/Graph.java
+++
@@ -1,9 +0,0 @@
-package org.sergei.collections.graph;
-
-import java.util.List;
-
-public class Graph {
-    List<Vertex> nodeList;
-    List<Edge> nodeEdgeList;
-
-}
--- investigation/src/main/java/org/sergei/investigation/abstraction/IActionsTwo.java
+++
@@ -1,9 +0,0 @@
-package org.sergei.investigation.abstraction;
-
-public interface IActionsTwo {
-    
-    String helloTwo();
-    
-    String helloThree();
-    
-}
